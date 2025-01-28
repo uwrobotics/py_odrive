ba81767b7dcc832a9b97cd3493c13cfcf5ca858a
y29lin <y29lin@uwaterloo.ca>
2025-01-28 16:56:55 -0500

    f

---
+++ test/test_dbc.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+import can
+import cantools
+
+@pytest.fixture
+def dbc_db():
+    return cantools.database.load_file("test/test_asset/odrive-cansimple.dbc")
+
+@pytest.fixture
+def AXIS_STATE_FULL_CALIBRATION_SEQUENCE():
+    return b'\x03\x00\x00\x00\x00\x00\x00\x00'
+
+def AXIS_STATE_FULL_CALIBRATION_SEQUENCE_CMD():
+    return can.Message(timestamp=0.0, arbitration_id=0xc7, is_extended_id=False, dlc=8, data=[0x3, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0])
+
+@pytest.fixture
+def axisID():
+    return 6
+
+def test_axis_calibration(dbc_db, axisID, AXIS_STATE_FULL_CALIBRATION_SEQUENCE, AXIS_STATE_FULL_CALIBRATION_SEQUENCE_CMD):
+    msg = dbc_db.get_message_by_name('Set_Axis_State')
+    payload = msg.encode({'Axis_Requested_State': 0x03})
+    assert payload == AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+    msg = can.Message(arbitration_id=msg.frame_id | axisID << 5, is_extended_id=False, data=payload)
+    assert msg == AXIS_STATE_FULL_CALIBRATION_SEQUENCE_CMD

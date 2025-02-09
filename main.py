afef3b441519e327abeb9880a50ed12264b27478
billylin609 <linyuchen830@gmail.com>
2025-02-09 16:11:18 -0500

    f

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from py_odrive.lib.odrive_wrapper import OdriveEncode
+from py_odrive.lib.utils import ProcessYaml, CanDevice
+import can 
 
-test = OdriveEncode('/home/linyuchen/ODrive-Driver/config/odrive-cansimple.dbc', use_jointconfig = False)
-print(test.Reboot(1))
+config = ProcessYaml('config/config.yaml')
+interface = config.get_config(key='interface', device_name='Drivetrain')
+channel = config.get_config(key='channel', device_name='Drivetrain')
+bitrate = config.get_config(key='bitrate', device_name='Drivetrain')
+
+bus = can.interface.Bus(interface=interface, channel=channel, bitrate=bitrate)
+
+db = OdriveEncode('config/odrive-cansimple.dbc', use_jointconfig = False)
+msg = db.Set_Axis_State(1, 'FULL_CALIBRATION_SEQUENCE')
+
+bus.send(msg)

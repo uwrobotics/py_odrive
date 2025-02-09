9fc8399b9313e83aa5c167a0e04a627cf7b792cd
yuchen <72412259+billylin609@users.noreply.github.com>
2025-02-09 16:59:43 -0500

    [odrive] encode function

---
+++ main.py
@@ -0,0 +1,20 @@
+from py_odrive.lib.odrive_wrapper import OdriveEncode
+from py_odrive.lib.utils import ProcessYaml, CanDevice
+import can
+import time
+
+config = ProcessYaml('config/config.yaml')
+interface = config.get_config(key='interface', device_name='Drivetrain')
+channel = config.get_config(key='channel', device_name='Drivetrain')
+bitrate = config.get_config(key='bitrate', device_name='Drivetrain')
+
+bus = can.interface.Bus(interface=interface, channel=channel, bitrate=bitrate)
+db = OdriveEncode('config/odrive-cansimple.dbc', use_jointconfig = False)
+msg = db.Set_Axis_State(2, 'FULL_CALIBRATION_SEQUENCE')
+bus.send(msg)
+time.sleep(30)
+msg = db.Set_Axis_State(2, 'CLOSED_LOOP_CONTROL')
+bus.send(msg)
+msg = db.Set_Input_Vel(2,0,0)
+print(msg)
+bus.send(msg)

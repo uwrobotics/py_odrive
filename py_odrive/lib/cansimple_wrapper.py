052ef0a29fc0b99c83f10b834384851e70827ece
billylin609 <linyuchen830@gmail.com>
2025-02-09 04:17:03 -0500

    f

--- py_odrive/lib/cansimple_wrapper.py
+++ py_odrive/lib/cansimple_wrapper.py
@@ -17,18 +17,25 @@ class CanWrapperEncode:
         self.db = cantools.database.load_file(dbc_filepath)
         self.encode_arbitration_id = lambda axis_id, msg_id: axis_id << 5 | msg_id
     
-    def _encode(self, axis_id, cmd: str, payload: Optional[dict] = None):
+    def _encode(self, axis_id, cmd: str, payload: Optional[dict] = None, rtr: Optional[bool]=False):
         msg = self.db.get_message_by_name(cmd)
         if msg.length == 0:
             assert payload is None
         else:
             data = self._encode_payload(msg, payload)
-        return can.Message(arbitration_id=self.encode_arbitration_id(axis_id, msg.frame_id), is_extended_id=False, data=data)
+        return can.Message(arbitration_id=self.encode_arbitration_id(axis_id, msg.frame_id), is_extended_id=False, is_remote_frame = rtr, data=data)
             
     def _encode_payload(self, msg, payload):
+        '''
+        Payload accept string or numbers check existence before assignment
+        '''
         dct = {}
         for signal in msg.signals:
             try:
+                if signal.choices is not None:
+                    assert payload[signal.name] in signal.choices.values() or payload[signal.name] in signal.choices
+                else:
+                    assert isinstance(payload[signal.name], int)
                 dct[signal.name] = payload[signal.name]
             except KeyError:
                 dct[signal.name] = 0
--- py_odrive/lib/odrive_wrapper.py
+++ py_odrive/lib/odrive_wrapper.py
@@ -8,6 +8,24 @@ class OdriveEncode(CanWrapperEncode):
     def set_estop(self, joint):
         axis_id = self.joint2axis(joint)
         return self._encode(axis_id, 'Estop')
+    
+    def get_Motor_Error(self, joint):
+        axis_id = self.joint2axis(joint)
+        return self._encode(axis_id, 'Get_Motor_Error', rtr=True)
+        
+    def get_Encoder_Error(self, joint):
+        axis_id = self.joint2axis(joint)
+        return self._encode(axis_id, 'Get_Encoder_Error', rtr=True)   
+     
+    def Get_Sensorless_Error(self, joint):
+        axis_id = self.joint2axis(joint)
+        return self._encode(axis_id, 'Get_Sensorless_Error', rtr=True)
+    
+    def Set_Axis_Node_ID(self, joint, id):
+        '''Use this method with caution'''
+        axis_id = self.joint2axis(joint)
+        payload = {'Axis_Node_ID': id}
+        return self._encode(axis_id, 'Get_Sensorless_Error', payload=payload)
         
     @staticmethod
     def joint2axis(joint):

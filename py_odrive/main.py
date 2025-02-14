da696e1c3c30aa72f4a531482bd6a870391c4e85
billylin609 <linyuchen830@gmail.com>
2025-02-14 13:04:54 -0500

    f

--- py_odrive/main.py
+++ py_odrive/main.py
@@ -1,30 +1,33 @@
-from uwrt_ros_msg.srv import OdriveCmd
-
+from uwrt_ros_msg.msg import OdriveCmd
 import rclpy
 from rclpy.node import Node
 
-
-class OdriveMsgService(Node):
+class OdriveMsgSubscriber(Node):
 
     def __init__(self):
-        super().__init__('odrive_msg_service')
-        self.srv = self.create_service(OdriveCmd, 'OdriveCmd', self.odrive_cmd_callback)
+        super().__init__('odrive_msg_subscriber')
+        self.subscription = self.create_subscription(
+            OdriveCmd,
+            'OdriveCmd',
+            self.odrive_cmd_callback,
+            10  # QoS history depth
+        )
+        # Prevent unused variable warning.
+        self.subscription
 
-    def odrive_cmd_callback(self, request, response):
-        print(request.axis_id, request.cmd, request.payload)
-        response.status = True
-        return response
+    def odrive_cmd_callback(self, msg):
+        # Print the received message fields.
+        print("Axis ID:", msg.axis_id)
+        print("Command:", msg.cmd)
+        print("Payload:", msg.payload)
 
 
 def main():
     rclpy.init()
-
-    msg_service = OdriveMsgService()
-
-    rclpy.spin(msg_service)
-
+    node = OdriveMsgSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()

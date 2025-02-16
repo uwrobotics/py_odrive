55da4b3ae41a18e82f95c1b52c8e3257ff0b41ce
y29lin <y29lin@uwaterloo.ca>
2025-02-16 02:51:21 -0500

    f

--- py_odrive/main.py
+++ py_odrive/main.py
@@ -1,13 +1,20 @@
-from uwrt_ros_msg.msg import OdriveCmd, MsgResponse
 import rclpy
 from rclpy.node import Node
+
 import datetime
+import os
+import can
+
+from lib.utils import CanDevice, ProcessYaml
+
+from uwrt_ros_msg.msg import OdriveCmd, MsgResponse
 
 
 class OdriveMsgSubscriber(Node):
 
     def __init__(self):
         super().__init__('odrive_msg_subscriber')
+        # Ros Messenger setup
         self.subscription = self.create_subscription(
             OdriveCmd,
             'OdriveCmd',
@@ -19,9 +26,24 @@ class OdriveMsgSubscriber(Node):
             'MsgResponse',
             10  # QoS history depth
         )
-        # Prevent unused variable warning.
         self.subscription
 
+    def can_setup(self):
+        # Read Yaml Config
+        yaml_dct = ProcessYaml('../config/config.yaml')
+        dct_key = yaml_dct.get_config().keys()
+        device_annotation = {}
+        for description in dct_key:
+            try:
+                interface = yaml_dct.get_config(key='interface', device_name=description)
+                channel = yaml_dct.get_config(key='channel', device_name=description)
+                bitrate = yaml_dct.get_config(key='bitrate', device_name=description)
+                bus = can.interface.Bus(interface=interface, channel=channel, bitrate=bitrate)
+                device_annotation[description] = yaml_dct.get_config(key='mapping', device_name=description)
+            except CanError:
+                pass
+            #log the result
+    
     def odrive_cmd_callback(self, msg):
         # Print the received message fields.
         print("Axis ID:", msg.axis_id)

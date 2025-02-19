9ea53105606cfbaf9571078237f1deb97841047a
billylin609 <linyuchen830@gmail.com>
2025-02-19 00:57:50 -0500

    f

--- py_odrive/main.py
+++ py_odrive/main.py
@@ -102,8 +102,6 @@ class OdriveMsgSubscriber(Node):
             if bus is not None:
                 bus.send(buf)
             print(buf)
-        if pending == False:
-            time.sleep(20)
         self.response_pub(json_msg['Stage'], json_msg['Target'], 'Success')
             
         

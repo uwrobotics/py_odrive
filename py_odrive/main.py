743f74e2d59a996c382e2a32fc643d070fe5b465
yuchen <72412259+billylin609@users.noreply.github.com>
2025-02-26 00:32:41 -0500

    Update main.py

--- py_odrive/main.py
+++ py_odrive/main.py
@@ -92,7 +92,10 @@ class OdriveMsgSubscriber(Node):
                         can_msg.append(self.db.Set_Controller_Mode(int(axis_id), command['control_mode'], command['input_mode']))
                 elif json_msg['Command'] == 'Set_Input_Vel':
                     for axis_id, command in json_msg['Payload'].items():
-                        can_msg.append(self.db.Set_Input_Vel(int(axis_id), float(command), 0))
+                        if axis_id in ['2', '4']:
+                            can_msg.append(self.db.Set_Input_Vel(int(axis_id), -float(command), 0))
+                        else:
+                            can_msg.append(self.db.Set_Input_Vel(int(axis_id), float(command), 0))
                 if 'Target' in json_msg:
                     bus = self.device_instance[json_msg['Target']].get_bus()
         print(bus)
